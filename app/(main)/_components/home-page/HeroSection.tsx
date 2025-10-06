86bdfd3dc04f403e1a5b1d989a9b2b625b0faa65
sakil-sagor <sakilsagor76543@gmail.com>
2025-10-06 18:54:02 +0600

    hero done

--- app/(main)/_components/home-page/HeroSection.tsx
+++ app/(main)/_components/home-page/HeroSection.tsx
@@ -11,13 +11,13 @@ const HeroSection = () => {
         <img
           className="h-full w-full object-cover"
           alt="Chicago skyline"
-          src="https://c.animaapp.com/mgdxk0bzO3P1u7/img/chicago-skyline-panorama-aerial-view-with-skyscrapers-lake-michi.png"
+          src="/assets/images/hero-bg.png"
         />
-        <div className="absolute inset-0 bg-[#00000080]" />
+        <div className="absolute inset-0 bg-[#0000002c]" />
       </div>
 
       {/* Content Container */}
-      <div className="relative z-10 mx-auto max-w-7xl px-4 py-20 lg:px-8">
+      <div className="relative z-10 container mx-auto px-2 py-20 lg:px-8">
         <div className="grid min-h-[600px] grid-cols-1 items-center gap-12 lg:grid-cols-2">
           {/* Left Content */}
           <div className="space-y-8">
