441984b9170211c7cae01aaef57806fa5cb4ca4b
sakil-sagor <sakilsagor76543@gmail.com>
2025-10-06 22:28:44 +0600

    fix card issue

--- app/(main)/_components/home-page/ServiceSection.tsx
+++ app/(main)/_components/home-page/ServiceSection.tsx
@@ -168,7 +168,7 @@ const ServiceSection = () => {
               </div>
             </div>
 
-            <Button className="mx-auto flex h-[60px] w-[260px] rounded-[40px] bg-primary shadow-[inset_0px_-1px_0px_#00000040] transition-colors hover:bg-[#c01234]">
+            <Button className="mx-auto flex w-[260px] rounded-[40px] bg-primary py-6 shadow-[inset_0px_-1px_0px_#00000040] transition-colors hover:bg-[#c01234]">
               <span className="text-base leading-[20.8px] font-semibold tracking-[0] text-white">
                 Book Your Ride
               </span>
